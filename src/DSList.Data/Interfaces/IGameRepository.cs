ddbaf0867c62ac0cfa5e70884908f94ff798ec1e
Keoma Kevin Baudin <keoma4@gmail.com>
2023-07-16 14:17:39 -0300

    Implement FindAllAsync

--- src/DSList.Data/Interfaces/IGameRepository.cs
+++ src/DSList.Data/Interfaces/IGameRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace DSList.Data.Interfaces
+﻿using DSList.Data.Entities;
+
+namespace DSList.Data.Interfaces
 {
     public interface IGameRepository
     {
+        Task<IEnumerable<Game>> FindAllAsync();
     }
 }
--- src/DSList.Data/Repositories/GameReposiotry.cs
+++ src/DSList.Data/Repositories/GameReposiotry.cs
@@ -1,11 +1,18 @@
-﻿using DSList.Data.Interfaces;
+﻿using DSList.Data.Entities;
+using DSList.Data.Interfaces;
 
 namespace DSList.Data.Repositories
 {
     public class GameReposiotry : IGameRepository
     {
+
         public GameReposiotry()
         {
+        }
+
+        public async Task<IEnumerable<Game>> FindAllAsync()
+        {
+            return new List<Game>();
         }
     }
 }
