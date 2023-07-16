e9c6bb38167662b5ce1ee9e6c97920212f6331cd
Keoma Kevin Baudin <keoma4@gmail.com>
2023-07-16 15:54:43 -0300

    Implement failing test - create game controller

---
+++ test/DSList.API.Test/GameControllerTests.cs
@@ -0,0 +1,16 @@
+﻿namespace DSList.API.Test
+{
+    public class GameControllerTests
+    {
+        [Fact]
+        public async Task FindAll_GetAction_ShouldReturnStatusOkAndGameMinDtoList()
+        {
+            // Arrange
+            var controller = new GameController();
+
+            // Act
+
+            // Assert
+        }
+    }
+}
