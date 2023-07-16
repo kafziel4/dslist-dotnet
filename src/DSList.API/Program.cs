30c17cc922273986a73dc31f04ac4418230acd77
Keoma Kevin Baudin <keoma4@gmail.com>
2023-07-16 20:04:56 -0300

    Remove DbContext registration from Program class

--- src/DSList.API/Program.cs
+++ src/DSList.API/Program.cs
@@ -1,6 +1,3 @@
-using DSList.Data.DbContexts;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,17 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-if (builder.Environment.IsDevelopment())
-{
-    builder.Services.AddDbContext<GameDbContext>(options =>
-        options.UseSqlite(builder.Configuration.GetConnectionString("GameDBConnectionString")));
-}
-else
-{
-    builder.Services.AddDbContext<GameDbContext>(options =>
-        options.UseNpgsql(builder.Configuration.GetConnectionString("GameDBConnectionString")));
-}
 
 var app = builder.Build();
 
