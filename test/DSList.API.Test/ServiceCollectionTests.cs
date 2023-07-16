ff85b050cd6ade2c515ae42a11cc0268ee6b1bfc
Keoma Kevin Baudin <keoma4@gmail.com>
2023-07-16 17:47:42 -0300

    Add failing test - register data services

---
+++ test/DSList.API.Test/ServiceCollectionTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DSList.API.Test
+{
+    public class ServiceCollectionTests
+    {
+        [Fact]
+        public void RegisterDataServices_EnvironmentIsDevelopment_ShouldRegisterGameDataServices()
+        {
+            // Arrange
+            var serviceCollection = new ServiceCollection();
+
+            // Act
+            serviceCollection.RegisterDataServices();
+
+            // Assert
+        }
+    }
+}
