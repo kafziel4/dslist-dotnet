0ed0c190d32dd7ad01e98bea1a13faa5390798ff
Keoma Kevin Baudin <keoma4@gmail.com>
2023-07-16 14:05:22 -0300

    Add failing test - create game repository

---
+++ test/DSList.Data.Test/GameRepositoryTests.cs
@@ -0,0 +1,16 @@
+﻿namespace DSList.Data.Test
+{
+    public class GameRepositoryTests
+    {
+        [Fact]
+        public async Task FindAllAsync_Invoke_ShouldReturnGameList()
+        {
+            // Arrange
+            var repository = new GameRepository();
+
+            // Act
+
+            // Assert
+        }
+    }
+}
