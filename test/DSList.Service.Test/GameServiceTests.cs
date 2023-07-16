b466f03cdd02510ba56156056748abdc6883a02a
Keoma Kevin Baudin <keoma4@gmail.com>
2023-07-16 14:59:18 -0300

    Implement failing test - create game service

---
+++ test/DSList.Service.Test/GameServiceTests.cs
@@ -0,0 +1,16 @@
+﻿namespace DSList.Service.Test
+{
+    public class GameServiceTests
+    {
+        [Fact]
+        public async Task FindAllAsync_Invoke_ShouldReturnGameMinDtoList()
+        {
+            // Arrange
+            var service = new GameService();
+
+            // Act
+
+            // Assert
+        }
+    }
+}
