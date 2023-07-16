f770012376be678c5cf67784b5694313472d6788
Keoma Kevin Baudin <keoma4@gmail.com>
2023-07-16 15:21:39 -0300

    Update failing test - add result assertions

--- test/DSList.Service.Test/GameServiceTests.cs
+++ test/DSList.Service.Test/GameServiceTests.cs
@@ -1,4 +1,6 @@
-﻿using DSList.Service.Services;
+﻿using DSList.Service.Dtos;
+using DSList.Service.Services;
+using FluentAssertions;
 
 namespace DSList.Service.Test
 {
@@ -9,11 +11,21 @@ namespace DSList.Service.Test
         {
             // Arrange
             var service = new GameService();
+            var expectedFirstGame = new GameMinDto
+            {
+                Id = 1,
+                Title = "Mass Effect Trilogy",
+                Year = 2012,
+                ImgUrl = "https://raw.githubusercontent.com/devsuperior/java-spring-dslist/main/resources/1.png",
+                ShortDescription = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit esse officiis corrupti unde repellat non quibusdam! Id nihil itaque ipsum!"
+            };
 
             // Act
             var result = await service.FindAllAsync();
 
             // Assert
+            result.Should().HaveCount(10);
+            result.First().Should().BeEquivalentTo(expectedFirstGame, options => options.ComparingByMembers<GameMinDto>());
         }
     }
 }
