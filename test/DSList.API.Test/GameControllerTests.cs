1aae7b54c7c16424396107323fc167e15d2be7bb
Keoma Kevin Baudin <keoma4@gmail.com>
2023-07-16 16:23:00 -0300

    Update failing test - add result assertions

--- test/DSList.API.Test/GameControllerTests.cs
+++ test/DSList.API.Test/GameControllerTests.cs
@@ -1,4 +1,7 @@
 ﻿using DSList.API.Controllers;
+using DSList.Service.Dtos;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DSList.API.Test
 {
@@ -9,11 +12,24 @@ namespace DSList.API.Test
         {
             // Arrange
             var controller = new GameController();
+            var expectedFirstGame = new GameMinDto
+            {
+                Id = 1,
+                Title = "Mass Effect Trilogy",
+                Year = 2012,
+                ImgUrl = "https://raw.githubusercontent.com/devsuperior/java-spring-dslist/main/resources/1.png",
+                ShortDescription = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit esse officiis corrupti unde repellat non quibusdam! Id nihil itaque ipsum!"
+            };
 
             // Act
             var result = await controller.FindAll();
 
             // Assert
+            var actionResult = result.Should().BeOfType<ActionResult<IEnumerable<GameMinDto>>>().Subject;
+            var okObjectResult = actionResult.Result.Should().BeOfType<OkObjectResult>().Subject;
+            var dtos = okObjectResult.Value.Should().BeAssignableTo<IEnumerable<GameMinDto>>().Subject;
+            dtos.Should().HaveCount(10);
+            dtos.First().Should().BeEquivalentTo(expectedFirstGame, options => options.ComparingByMembers<GameMinDto>());
         }
     }
 }
