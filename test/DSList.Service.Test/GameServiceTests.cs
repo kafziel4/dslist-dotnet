e6147d0af3e703043371a0ccdbe6b9e51783b4d3
Keoma Kevin Baudin <keoma4@gmail.com>
2023-07-16 23:30:29 -0300

    Implement failing test - find game by id

--- test/DSList.Service.Test/GameServiceTests.cs
+++ test/DSList.Service.Test/GameServiceTests.cs
@@ -58,5 +58,54 @@ namespace DSList.Service.Test
             result.Should().HaveCount(10);
             result.First().Should().BeEquivalentTo(expectedFirstGame, options => options.ComparingByMembers<GameMinDto>());
         }
+
+        [Fact]
+        public async Task FindByIdAsync_Invoke_ShouldReturnGameDto()
+        {
+            // Arrange
+            var game = new Game
+            {
+                Id = 1,
+                Title = "Mass Effect Trilogy",
+                Score = 4.8,
+                Year = 2012,
+                Genre = "Role-playing (RPG), Shooter",
+                Platforms = "XBox, Playstation, PC",
+                ImgUrl = "https://raw.githubusercontent.com/devsuperior/java-spring-dslist/main/resources/1.png",
+                ShortDescription = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit esse officiis corrupti unde repellat non quibusdam! Id nihil itaque ipsum!",
+                LongDescription = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus dolorum illum placeat eligendi, quis maiores veniam. " +
+                    "Incidunt dolorum, nisi deleniti dicta odit voluptatem nam provident temporibus reprehenderit blanditiis consectetur tenetur. " +
+                    "Dignissimos blanditiis quod corporis iste, aliquid perspiciatis architecto quasi tempore ipsam voluptates ea ad distinctio, sapiente qui, amet quidem culpa."
+            };
+
+            var repositoryMock = new Mock<IGameRepository>();
+            repositoryMock.Setup(_ => _.FindByIdAsync(1)).ReturnsAsync(game);
+
+            var mapperConfiguration = new MapperConfiguration(cfg =>
+                cfg.AddProfile<GameProfile>());
+            var mapper = new Mapper(mapperConfiguration);
+
+            var service = new GameService(repositoryMock.Object, mapper);
+            var expectedGame = new GameDto
+            {
+                Id = 1,
+                Title = "Mass Effect Trilogy",
+                Score = 4.8,
+                Year = 2012,
+                Genre = "Role-playing (RPG), Shooter",
+                Platforms = "XBox, Playstation, PC",
+                ImgUrl = "https://raw.githubusercontent.com/devsuperior/java-spring-dslist/main/resources/1.png",
+                ShortDescription = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit esse officiis corrupti unde repellat non quibusdam! Id nihil itaque ipsum!",
+                LongDescription = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus dolorum illum placeat eligendi, quis maiores veniam. " +
+                    "Incidunt dolorum, nisi deleniti dicta odit voluptatem nam provident temporibus reprehenderit blanditiis consectetur tenetur. " +
+                    "Dignissimos blanditiis quod corporis iste, aliquid perspiciatis architecto quasi tempore ipsam voluptates ea ad distinctio, sapiente qui, amet quidem culpa."
+            };
+
+            // Act
+            var result = await service.FindByIdAsync(1);
+
+            // Assert
+            result.Should().BeEquivalentTo(expectedGame, options => options.ComparingByMembers<GameDto>());
+        }
     }
 }
