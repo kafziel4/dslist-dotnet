49bc617a2921c4e99f50780a173ee5afba6e3e39
Keoma Kevin Baudin <keoma4@gmail.com>
2023-07-16 21:18:14 -0300

    Register Services on Program class

--- src/DSList.API/Program.cs
+++ src/DSList.API/Program.cs
@@ -1,3 +1,5 @@
+using DSList.API;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.RegisterBusinessServices();
+builder.Services.RegisterDataServices(builder.Configuration, builder.Environment);
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
