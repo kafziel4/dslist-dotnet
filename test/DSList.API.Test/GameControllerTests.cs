053044cf4f8802254a8a02f7e18373680416c2c1
Keoma Kevin Baudin <keoma4@gmail.com>
2023-07-16 23:47:01 -0300

    Implement failing test - find by id

--- test/DSList.API.Test/GameControllerTests.cs
+++ test/DSList.API.Test/GameControllerTests.cs
@@ -48,5 +48,53 @@ namespace DSList.API.Test
             dtos.Should().HaveCount(10);
             dtos.First().Should().BeEquivalentTo(expectedFirstGame, options => options.ComparingByMembers<GameMinDto>());
         }
+
+        [Fact]
+        public async Task FindById_GetAction_ShouldReturnStatusOkAndGameDto()
+        {
+            // Arrange
+            var game = new GameDto
+            {
+                Id = 1,
+                Title = "Mass Effect Trilogy",
+                Score = 4.8,
+                Year = 2012,
+                Genre = "Role-playing (RPG), Shooter",
+                Platforms = "XBox, Playstation, PC",
+                ImgUrl = "https://raw.githubusercontent.com/devsuperior/java-spring-dslist/main/resources/1.png",
+                ShortDescription = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit esse officiis corrupti unde repellat non quibusdam! Id nihil itaque ipsum!",
+                LongDescription = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus dolorum illum placeat eligendi, quis maiores veniam. " +
+                    "Incidunt dolorum, nisi deleniti dicta odit voluptatem nam provident temporibus reprehenderit blanditiis consectetur tenetur. " +
+                    "Dignissimos blanditiis quod corporis iste, aliquid perspiciatis architecto quasi tempore ipsam voluptates ea ad distinctio, sapiente qui, amet quidem culpa."
+            };
+
+            var gameServiceMock = new Mock<IGameService>();
+            gameServiceMock.Setup(_ => _.FindByIdAsync(1)).ReturnsAsync(game);
+
+            var controller = new GameController(gameServiceMock.Object);
+            var expectedFirstGame = new GameDto
+            {
+                Id = 1,
+                Title = "Mass Effect Trilogy",
+                Score = 4.8,
+                Year = 2012,
+                Genre = "Role-playing (RPG), Shooter",
+                Platforms = "XBox, Playstation, PC",
+                ImgUrl = "https://raw.githubusercontent.com/devsuperior/java-spring-dslist/main/resources/1.png",
+                ShortDescription = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit esse officiis corrupti unde repellat non quibusdam! Id nihil itaque ipsum!",
+                LongDescription = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus dolorum illum placeat eligendi, quis maiores veniam. " +
+                    "Incidunt dolorum, nisi deleniti dicta odit voluptatem nam provident temporibus reprehenderit blanditiis consectetur tenetur. " +
+                    "Dignissimos blanditiis quod corporis iste, aliquid perspiciatis architecto quasi tempore ipsam voluptates ea ad distinctio, sapiente qui, amet quidem culpa."
+            };
+
+            // Act
+            var result = await controller.FindById(1);
+
+            // Assert
+            var actionResult = result.Should().BeOfType<ActionResult<GameDto>>().Subject;
+            var okObjectResult = actionResult.Result.Should().BeOfType<OkObjectResult>().Subject;
+            var dto = okObjectResult.Value.Should().BeAssignableTo<GameDto>().Subject;
+            dto.Should().BeEquivalentTo(expectedFirstGame, options => options.ComparingByMembers<GameMinDto>());
+        }
     }
 }
