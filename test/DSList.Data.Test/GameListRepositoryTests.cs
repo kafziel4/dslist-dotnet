54df2fe408d6536cf5c8b7ce527bcb2182b57ea1
Keoma Kevin Baudin <keoma4@gmail.com>
2023-07-17 23:31:35 -0300

    Implement failing test - find all game lists

---
+++ test/DSList.Data.Test/GameListRepositoryTests.cs
@@ -0,0 +1,34 @@
+﻿using DSList.Data.DbContexts;
+using DSList.Data.Entities;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace DSList.Data.Test
+{
+    public class GameListRepositoryTests
+    {
+        [Fact]
+        public async Task FindAllAsync_Invoke_ShouldReturnListOfGameList()
+        {
+            // Arrange
+            var connection = new SqliteConnection("Data Source=:memory:");
+            connection.Open();
+
+            var optionsBuilder = new DbContextOptionsBuilder<GameDbContext>().UseSqlite(connection);
+            var context = new GameDbContext(optionsBuilder.Options);
+            context.Database.EnsureCreated();
+
+            var repository = new GameListRepository(context);
+
+            var expectedGameList = new GameList
+            {
+                Id = 1,
+                Name = "Aventura e RPG"
+            };
+
+            // Act
+
+            // Assert
+        }
+    }
+}
