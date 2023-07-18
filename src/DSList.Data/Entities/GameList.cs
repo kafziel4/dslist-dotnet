f3eccd9ddebf7b6469f82dcbac89da0cebb1d76f
Keoma Kevin Baudin <keoma4@gmail.com>
2023-07-17 22:18:20 -0300

    Add GameList entity

---
+++ src/DSList.Data/Entities/GameList.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DSList.Data.Entities
+{
+    public class GameList
+    {
+        public long Id { get; set; }
+
+        [StringLength(255)]
+        public string Name { get; set; } = string.Empty;
+
+        public GameList()
+        {
+        }
+
+        public GameList(long id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is GameList list &&
+                   Id == list.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+    }
+}
