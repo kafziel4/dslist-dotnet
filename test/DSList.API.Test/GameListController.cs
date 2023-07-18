0024fac178fa66d8c6b420306589b6354679e200
Keoma Kevin Baudin <keoma4@gmail.com>
2023-07-18 00:12:42 -0300

    Implement failing test - game list controller

---
+++ test/DSList.API.Test/GameListController.cs
@@ -0,0 +1,33 @@
+﻿using DSList.Service.DTOs;
+using DSList.Service.Interfaces;
+using Moq;
+
+namespace DSList.API.Test
+{
+    public class GameListControllerTests
+    {
+        [Fact]
+        public async Task FindAll_GetAction_ShouldReturnStatusOkAndListOfGameListDto()
+        {
+            // Arrange
+            var mockService = new Mock<IGameListService>();
+
+            var listOfGameList = new List<GameListDto>();
+            for (int i = 1; i <= 2; i++)
+            {
+                listOfGameList.Add(new GameListDto
+                {
+                    Id = i,
+                    Name = "Aventura e RPG"
+                });
+            }
+            mockService.Setup(_ => _.FindAllAsync()).ReturnsAsync(listOfGameList);
+
+            var controller = new GameListController(mockService.Object);
+
+            // Act
+
+            // Assert
+        }
+    }
+}
