edaab3be88d5f95baa0e0ad1dd6e5e66fec1d989
Keoma Kevin Baudin <keoma4@gmail.com>
2023-07-17 23:50:21 -0300

    Implement failing test - find all async

---
+++ test/DSList.Service.Test/GameListServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using DSList.Data.Entities;
+using DSList.Data.Interfaces;
+using Moq;
+
+namespace DSList.Service.Test
+{
+    public class GameListServiceTests
+    {
+        [Fact]
+        public async Task FindAllAsync_Invoke_ShouldReturnListOfGameListDto()
+        {
+            // Arrange
+            var mockRepository = new Mock<IGameListRepository>();
+
+            var listOfGameList = new List<GameList>();
+            for (int i = 1; i <= 2; i++)
+            {
+                listOfGameList.Add(new GameList
+                {
+                    Id = i,
+                    Name = "Aventura e RPG"
+                });
+            }
+            mockRepository.Setup(_ => _.FindAllAsync()).ReturnsAsync(listOfGameList);
+
+            var service = new GameListService(mockRepository.Object);
+
+            // Act          
+
+            // Assert
+        }
+    }
+}
